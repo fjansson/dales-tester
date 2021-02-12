254c7b27890af035c0bd007521c8ec54b95d8ade
Fredrik Jansson <fjansson@abo.fi>
2021-02-13 00:42:54 +0900

    fix Fugaku compile script

--- compile-fugaku.sh
+++ compile-fugaku.sh
@@ -3,15 +3,20 @@
 TAG=$1
 
 
+# before directory shuffle end of 2020
+# does it also work after?
 . /vol0001/apps/oss/spack/share/spack/setup-env.sh
 spack load netcdf-fortran%fj
+spack load fftw@3.3.8%fj /4jakmbv
 spack load cmake@3.17.3 arch=linux-rhel7-haswell
 
+#. /vol0004/apps/oss/spack/share/spack/setup-env.sh
+#spack load netcdf-fortran%fj
+#spack load fftw@3.3.8%fj /xffcrpu
+#spack load cmake@3.17.3%gcc        # not packaged for intel arch on vol0004 spack
 
-# version 4.2.1 and below don't find netcdf on Cartesius
-# export NETCDF_INCLUDE=/sw/arch/RedHatEnterpriseServer7/EB_production/2019/software/netCDF-Fortran/4.4.4-foss-2018b/include
-# export NETCDF_LIB=/sw/arch/RedHatEnterpriseServer7/EB_production/2019/software/netCDF-Fortran/4.4.4-foss-2018b/lib
-
+# 4.3-rc.2 and earlier don't work on Fugaku. Support added in branches fugaku and to4.4_Fredrik
+#
 # Uses -DUSE_HYPRE=True and -DUSE_FFTW=True
 # versions before 4.3 do not support these, and will ignore them
 
