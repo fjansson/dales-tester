68720dd2435320c9fe6eb15f3537dd8b0802d112
Fredrik Jansson <fjansson@abo.fi>
2021-11-22 22:31:39 +0900

    update fugaku scripts to Sept 2021 tcsds-1.2.33 toolchain and system-wide spack

--- compile-fugaku.sh
+++ compile-fugaku.sh
@@ -2,27 +2,22 @@
 
 TAG=$1
 
+# system-wide spack - after Sept 2021 update, environment version '4.6.1 tcsds-1.2.33'
+# note uses spack-v0.16.2 path which is not officially documented (?)
+. /vol0004/apps/oss/spack-v0.16.2/share/spack/setup-env.sh
+spack load netcdf-fortran%fj /bubmb4i
+spack load fftw%fj
 
-# before directory shuffle end of 2020
-# obsolete 1. March 2021 when login nodes switching to RHEL 8
-#. /vol0001/apps/oss/spack/share/spack/setup-env.sh
-#spack load netcdf-fortran%fj
-#spack load fftw@3.3.8%fj /4jakmbv
-#spack load cmake@3.17.3 arch=linux-rhel7-haswell
+# local spack - used before Sept 2021
+# Trying to work around mixing of 1.2.29 and 1.2.31 environments
+# . ~/spack/share/spack/setup-env.sh
+# spack load netcdf-fortran%fj
+# spack load fftw%fj +openmp
 
-# switch to older language environment,
-# for Bus error reproduction test
-#module unload lang
-#module load lang/tcsds-1.2.29 
-
-. /vol0004/apps/oss/spack/share/spack/setup-env.sh
-spack load netcdf-fortran%fj
-spack load fj-fftw
-spack load cmake@3.18.4%gcc
 
 # workaround for library errors in git after loading spack
 # /usr/libexec/git-core/git-remote-https: symbol lookup error: /lib64/libk5crypto.so.3: undefined symbol: EVP_KDF_ctrl, version OPENSSL_1_1_1b
-export LD_LIBRARY_PATH=/lib64:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=/lib64:$LD_LIBRARY_PATH
 
 # DALES 4.3-rc.2 and earlier don't work on Fugaku.
 # Support added in branches fugaku and to4.4_Fredrik
@@ -51,6 +46,7 @@ cmake ../dales -DUSE_FFTW=True
 # -DUSE_HYPRE=True
 # -DHYPRE_LIB=
 
-make -j 4
+make -j 4 2>&1 | tee compilation-log.txt
+
 
 
