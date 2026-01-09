64253d7f476e51be12dd7fa773d74d7c8f8e4a63
Fredrik Jansson <fjansson@int6.local.snellius.surf.nl>
2026-01-09 14:07:22 +0100

    update snellius compile scripts

--- compile-snellius-dbg-genoa.sh
+++ compile-snellius-dbg-genoa.sh
@@ -2,13 +2,14 @@
 
 TAG=$1
 
+# not used in DALES >= 5.0
 export SYST=gnu-fast
 
-module load 2023
-module load foss/2023a
-module load netCDF-Fortran/4.6.1-gompi-2023a
-module load CMake/3.26.3-GCCcore-12.3.0
-module load Hypre/2.29.0-foss-2023a
+module load 2025
+module load foss/2025a
+module load netCDF-Fortran/4.6.2-gompi-2025a
+module load CMake/3.31.3-GCCcore-14.2.0
+#module load Hypre/2.33.0-foss-2025a
 
 # version 4.2.1 and below don't find netcdf on Cartesius
 #export NETCDF_INCLUDE=/sw/arch/RedHatEnterpriseServer7/EB_production/2019/software/netCDF-Fortran/4.4.4-foss-2018b/include
@@ -32,5 +33,3 @@ cd $BUILD
 cmake ../dales -DCMAKE_BUILD_TYPE=Debug
 
 make -j 4
-
-
--- compile-snellius-sp-genoa.sh
+++ compile-snellius-sp-genoa.sh
@@ -2,13 +2,16 @@
 
 TAG=$1
 
+# not used in DALES >= 5.0
 export SYST=gnu-fast
 
-module load 2023
-module load foss/2023a
-module load netCDF-Fortran/4.6.1-gompi-2023a
-module load CMake/3.26.3-GCCcore-12.3.0
-module load Hypre/2.29.0-foss-2023a
+
+module load 2025
+module load foss/2025a
+module load netCDF-Fortran/4.6.2-gompi-2025a
+module load CMake/3.31.3-GCCcore-14.2.0
+#module load Hypre/2.33.0-foss-2025a
+
 
 # version 4.2.1 and below don't find netcdf on Cartesius
 #export NETCDF_INCLUDE=/sw/arch/RedHatEnterpriseServer7/EB_production/2019/software/netCDF-Fortran/4.4.4-foss-2018b/include
@@ -29,8 +32,6 @@ mkdir $BUILD
 cd $BUILD
 
 #cmake ../dales -DUSE_HYPRE=True -DUSE_FFTW=True -DFIELD_PRECISION=32 -DPOIS_PRECISION=32
-cmake ../dales -DENABLE_FP32_FIELDS=ON -DENABLE_FP32_POIS=ON
+cmake ../dales -DENABLE_FP32_FIELDS=ON -DENABLE_FP32_POIS=ON -DENABLE_ZSTANDARD=ON
 
 make -j 8
-
-
